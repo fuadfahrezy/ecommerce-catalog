1fcb42b9a8fc4530b636d2d9e59fbf9021b20310
fuadfahrezy <fuadfahrezy>
2023-10-27 23:25:50 +0700

    add feature product, style and assets

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <Product/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from './components/Product.vue';
+
+Product
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Product
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/Product.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <!-- <div class="card">
+            <div class="container-product">
+                <div class="letak-gambar"></div>
+                <div class="letak-deskripsi">
+                    <div class="letak-deskripsi-top"></div>
+                    <div class="letak-deskripsi-bottom"></div>
+                </div>
+            </div>
+        </div> -->
+        <div class="container">
+            <div class="background">
+                <img src="../assets/bg-pattern.svg" alt="background">
+            </div>
+            <div class="card">
+                <div class="container-unavailable-product">
+                    <div class="background">
+                        <img src="../assets/sad-face.svg" alt="sad">
+                    </div>
+                    <div class="product-details">
+                        <p>This product is unavailable to show</p>
+                        <div class="tombol">
+                            <button type="button" class="tombol-next">Next Product</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    @import '../css/style.css'
+</style>
