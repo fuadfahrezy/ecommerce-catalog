4e3b12e6624d36acf4f191882be6cc1b0b67d403
fuadfahrezy <fuadfahrezy>
2023-10-29 11:15:41 +0700

    update product and style

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -14,7 +14,7 @@
                 <img src="../assets/bg-pattern.svg" alt="background">
             </div>
             <div class="card">
-                <div class="container-unavailable-product">
+                <!-- <div class="container-unavailable-product">
                     <div class="background">
                         <img src="../assets/sad-face.svg" alt="sad">
                     </div>
@@ -24,6 +24,21 @@
                             <button type="button" class="tombol-next">Next Product</button>
                         </div>
                     </div>
+                </div> -->
+                <div class="container-product">
+                    <div class="image-product">
+                        <img src="" alt="">
+                    </div>
+                    <div class="product-details">
+                        <div class="top">
+                            <h3 class="title">Men Casual Slim Fit</h3>
+                            <div class="sub-title">
+                                <div class="rate">
+                                    <p>men clothing</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -31,7 +46,19 @@
 </template>
 <script>
 export default {
-    
+    name: 'Product',
+    data() {
+        return {
+            
+        }
+    },
+    methods: {
+        async API(){
+            const response = await fetch('https://fakestoreapi.com/products');
+            const result = await response.json();
+            return result;
+        }
+    },
 }
 </script>
 <style scoped>
